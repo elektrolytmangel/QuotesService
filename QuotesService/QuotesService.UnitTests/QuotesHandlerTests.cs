208241a76accfceb57960b77cf89fd894a19db6d
MUEHLHEIM Marc <marc.muehlheim@hexagon.com>
2022-03-09 17:25:42 +0100

    Add test description

--- QuotesService/QuotesService.UnitTests/QuotesHandlerTests.cs
+++ QuotesService/QuotesService.UnitTests/QuotesHandlerTests.cs
@@ -13,6 +13,12 @@ namespace QuotesService.UnitTests
         [Test]
         public void Get_QuotesAvailable_ShouldReturnMiaWallaceQuote()
         {
+            // ToDo: add test to check if quote can be added
+            // arrange
+            // Already done:
+            // 1. instantiate QuotesHandler
+            // 2. prepare list of Quote with one quote and give an id which you know: https://guidgenerator.com/
+
             // arrange
             var handler = new QuotesHandler();
             Guid miasId = Guid.Parse("87f80773-f9f9-47d4-8210-bc84c8ae247f");
@@ -28,55 +34,54 @@ namespace QuotesService.UnitTests
             };
 
             // act
-            var queriedQuotes = handler.Get(miasId, allQuotes);
-            var miaWallaceQuote = queriedQuotes.SingleOrDefault();
+            // ToDo: 
+            // 1. Execute the action: QuotesHandler.Get with the known id
+            // 2. select the single result and assign it to an variable i.e. miaWallaceQuote
 
             // assert
-            Assert.That(miaWallaceQuote, Is.Not.Null);
-            Assert.That(miaWallaceQuote?.Content, Is.EqualTo("That's when you know you found somebody really special. When you just shut the fuck up for a minute and comfortably share silence."));
+            // ToDo:
+            // 1. Assert that the result of the action is not null
+            // 2. Assert that the result of the action has a content which is equal to "That's when you know you found somebody really special. When you just shut the fuck up for a minute and comfortably share silence."
+
+            // ToDo: Execute the test
         }
 
         [Test]
         public void AddOrUpdate_NewQuote_ShouldAddNewQuote()
         {
+            // ToDo: add test to check if quote can be added
             // arrange
-            var handler = new QuotesHandler();
-            var allQuotes = new List<Quote>();
-            var newQuote = new Quote
-            {
-                Content = "Aw, man. I shot Marvin in the face.",
-                Author = "Vincent Vega",
-                AdditionalInformation = "Pulp Fiction",
-                Type = QuoteType.FAMOUSE,
-            };
+            // ToDo:
+            // 1. instantiate QuotesHandler
+            // 2. prepare empty list of Quote
 
             // act
-            var quoteResult = handler.AddOrUpdate(newQuote, allQuotes);
+            // ToDo: 
+            // 1. Execute the action: QuotesHandler.AddOrUpdate
 
             // assert
-            Assert.That(quoteResult, Is.Not.Null);
-            Assert.That(quoteResult?.Id, Is.Not.Empty);
+            // ToDo:
+            // 1. Assert that the result of the action is not null
+            // 2. Assert that the result of the action has an id which is not empty
+
+            // ToDo: Execute the test
         }
 
         [Test]
         public void Delete_QuoteAvailable_ShouldDeleteQuote()
         {
+            // ToDo: add test to check if quote can be deleted
             // arrange
-            var handler = new QuotesHandler();
-            Guid miasId = Guid.Parse("87f80773-f9f9-47d4-8210-bc84c8ae247f");
-            var allQuotes = new List<Quote>
-            {
-                new Quote
-                {
-                    Id = miasId,
-                    Author = "Mia Wallace",
-                    Content = "That's when you know you found somebody really special. When you just shut the fuck up for a minute and comfortably share silence.",
-                    AdditionalInformation = "Pulp Fiction",
-                },
-            };
+            // ToDo:
+            // 1. instantiate QuotesHandler
+            // 2. prepare list of Quote with one quote and give an id which you know: https://guidgenerator.com/
 
             // act && assert
-            Assert.DoesNotThrow(() => handler.DeleteQuote(miasId, allQuotes));
+            // ToDo: 
+            // 1. Execute the action: QuotesHandler.DeleteQuote
+            // 2. Wrap it into your expected Assert: Assert.DoesNotThrow(() => YourDelete-Action);
+
+            // ToDo: Execute the test
         }
     }
 }
