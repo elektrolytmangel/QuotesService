3a05c3e5da2d9ee54977e0350b8d3893a994b349
MUEHLHEIM Marc <marc.muehlheim@hexagon.com>
2022-03-09 21:59:34 +0100

    Add string enum converter

--- QuotesService/QuotesService/Model/QuoteType.cs
+++ QuotesService/QuotesService/Model/QuoteType.cs
@@ -1,9 +1,12 @@
-﻿namespace QuotesService.Model
+﻿using System.Text.Json.Serialization;
+
+namespace QuotesService.Model
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum QuoteType
     {
         FAVORITE = 1,
         FAMOUSE = 2, 
-        SOMEONE_OTHERS_FAVORITE = 4,
+        SOMEONE_OTHERS_FAVORITE = 3,
     }
 }
